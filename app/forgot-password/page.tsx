704dcf94312752a65e685a87884d4acdbad32745
Hafis Babayev <riderblade1994@gmail.com>
2025-12-06 12:17:41 +0400

    Creeate forgot password

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import axios from "axios";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (status) {
+      const timer = setTimeout(() => {
+        setStatus(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [status]);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/forgot-password`,
+        { email }
+      );
+
+      setStatus("OTP code has been successfully sent to your email.");
+    } catch (err) {
+      setStatus("Failed to send OTP code. Please try again later.");
+      console.log("Error:", err);
+    }
+    setEmail("");
+  }
+
+  return (
+    <div className="flex flex-col gap-5 h-screen items-center text-primary justify-center">
+      <h1 className="text-3xl text-center">Forgot password</h1>
+      <p>Enter your email to reset your password</p>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center gap-5"
+      >
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          placeholder="you@example.com"
+          className="border border-primary bg-transparent w-80 px-2 py-2.5  outline-none max-w-150 rounded-xl"
+        />
+        {status && <span>{status}</span>}
+        <button className="bg-primary text-bg border py-2.5 w-80 px-5 font-semibold border-primary text-xl rounded-xl cursor-pointer hover:bg-transparent hover:text-primary transition duration-300">
+          Reset password
+        </button>
+      </form>
+    </div>
+  );
+}
